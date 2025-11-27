3cc285a411fab5632063baf3bb0fb6dff42b9852
samshx606 <saaameh.0.1@gamil.com>
2025-11-27 20:25:40 +0200

    refactor(user-profile): update field visibility and remove unnecessary annotations

--- src/main/java/com/icoder/user/management/dto/user/UserProfileResponse.java
+++ src/main/java/com/icoder/user/management/dto/user/UserProfileResponse.java
@@ -2,7 +2,6 @@ package com.icoder.user.management.dto.user;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,18 +13,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class UserProfileResponse {
-    @NotBlank
     private String handle;
-    @NotBlank
     private String nickname;
-    @NotBlank
     private String email;
     private String school;
     private String pictureUrl;
-    @NotBlank
     private boolean verified;
-    @NotBlank
     private int acceptedCount = 0;
-    @NotBlank
     private int attemptedCount = 0;
 }
