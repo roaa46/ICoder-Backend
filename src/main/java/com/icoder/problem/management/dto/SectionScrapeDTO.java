df05bb839ba8a1891921230272c94ceec2c7b271
Roaa Mohamed <roaaamohamed66@gmail.com>
2026-01-30 13:16:44 +0200

    feat(dto): replace `@JsonSerialize` with `@JsonFormat` for numeric fields in DTOs
    - Updated `@JsonSerialize(using = ToStringSerializer.class)` to `@JsonFormat(shape = JsonFormat.Shape.STRING)` for consistent numeric field serialization across all relevant DTO classes.

--- src/main/java/com/icoder/problem/management/dto/SectionScrapeDTO.java
+++ src/main/java/com/icoder/problem/management/dto/SectionScrapeDTO.java
@@ -1,10 +1,9 @@
 package com.icoder.problem.management.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,7 +17,7 @@ import java.util.List;
 @Builder
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class SectionScrapeDTO {
-    @JsonSerialize(using = ToStringSerializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     private Long sectionId;
     @JsonInclude(JsonInclude.Include.NON_NULL) // title is null in the problem statement section in CF & CSES
     private String title;
