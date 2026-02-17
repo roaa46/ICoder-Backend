043a6efec4a6d2cd0241cf08e212332aeebb67c2
Roaa Mohamed <roaaamohamed66@gmail.com>
2026-02-17 02:37:09 +0200

    feat(submission): enable specification-based query support in `SubmissionRepository`

--- src/main/java/com/icoder/submission/management/repository/SubmissionRepository.java
+++ src/main/java/com/icoder/submission/management/repository/SubmissionRepository.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -18,7 +19,7 @@ import java.util.Optional;
 import java.util.Set;
 
 @Repository
-public interface SubmissionRepository extends JpaRepository<Submission, Long> {
+public interface SubmissionRepository extends JpaRepository<Submission, Long>, JpaSpecificationExecutor<Submission> {
 
     Optional<Submission> findById(Long id);
 
