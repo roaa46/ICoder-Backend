a9729e4f190e1d6051d6475cdf437865fc35145c
Roaa Mohamed <roaaamohamed66@gmail.com>
2026-05-01 15:25:26 +0300

    Enhance caching logic and improve user service tests (#10)
    * test(user-service): mock cache dependencies and add cache validation in `updateProfile_shouldSaveUser` test
    
    * feat(ci): split Maven build, test, and package steps in workflow
    
    * test(user): add cache-related mocks and verify cache eviction in updateProfile test
    
    * fix(cache): update cache name in `getProblemStatement` method
    
    * feat(cache): enhance caching logic and improve test coverage
    
    - Added `@Cacheable` annotations in `UserServiceImpl` for `getProfile` and `updateProfile` methods.
    
    * test(user-service): mock cache dependencies and add cache validation in `updateProfile_shouldSaveUser` test

--- src/main/java/com/icoder/core/config/RedisConfig.java
+++ src/main/java/com/icoder/core/config/RedisConfig.java
@@ -1,6 +1,8 @@
 package com.icoder.core.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,6 +24,10 @@ public class RedisConfig {
     @Bean
     public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
         ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
         objectMapper.activateDefaultTyping(
                 objectMapper.getPolymorphicTypeValidator(),
                 ObjectMapper.DefaultTyping.NON_FINAL
